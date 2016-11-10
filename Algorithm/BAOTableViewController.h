81f4ad47429905133d74829a0c43b02c14382c35
田大宝 <tiandabao@le.com>
2016-11-10 10:07:11 +0800

    first commit

--- Algorithm/BAOTableViewController.h
+++ Algorithm/BAOTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  BAOTableViewController.h
 //  Algorithm
 //
 //  Created by dabao on 2016/11/10.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface BAOTableViewController : UITableViewController
 
 @end
-
